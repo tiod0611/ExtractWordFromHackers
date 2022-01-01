b9e0fb377af23395b43fbddaaad45e5a4be279f8
kyeul611 <gyul611@likelion.org>
2022-01-01 22:53:30 +0900

    update code

--- ExtractWords.py
+++ ExtractWords.py
@@ -1,22 +1,22 @@
 import pdftotext
 import pandas as pd
-# import csv
 import re
 import os
 
-
 class Extractword:
     
-
     def __init__(self):
         self.answersheet = []
         self.path = './test_papers'
+        self.stop = ['Powered by TCPDF wwwtcpdforg']
 
     def extractword(self, content):
         '''시험지에서 영단어를 추출'''
-        ext = re.sub("[^a-z\n\ -]","", content[1])
+
+        ext = re.sub("[0-9]", "\n", content[1])
+        ext = re.sub("[^A-Za-z\n\ -]","", ext)
         ext = re.sub(" +", " ", ext).split('\n')
-        ext = [word for word in ext if word != ' ' and word != '']
+        ext = [word.rstrip(' ') for word in ext if word != ' ' and word != '' and word not in self.stop]
         ext = list(filter(None, ext))[2:]
     
         if len(ext) > 20:
@@ -32,8 +32,9 @@ class Extractword:
         
         print("** File list **")
         for file in listdir:
-            print(file)
+
             if file.split('.')[-1] == 'pdf':
+                print(file)
                 with open(os.path.join(self.path, file), 'rb') as pdf:
                     fileReader = pdftotext.PDF(pdf)
                 self.extractword(fileReader)
